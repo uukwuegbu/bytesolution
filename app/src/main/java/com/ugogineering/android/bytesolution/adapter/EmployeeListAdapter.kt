940dfa8ea7af689f37d662de39e8e3e0040bd13a
ugogineering <ugogineering@gmail.com>
2021-09-15 07:06:48 +0100

    The EmployeeListAdapter has been refactored and the functionality is same as before refactoring. The objective of the refactoring is to separate the code to display the viewHolder and the code to manage the viewHolder

--- app/src/main/java/com/ugogineering/android/bytesolution/adapter/EmployeeListAdapter.kt
+++ app/src/main/java/com/ugogineering/android/bytesolution/adapter/EmployeeListAdapter.kt
@@ -18,35 +18,15 @@ class EmployeeListAdapter: RecyclerView.Adapter<EmployeeListAdapter.ViewHolder>(
     override fun getItemCount() = data.size
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        // val res holds a reference to the resources for this view
-        val res = holder.itemView.context.resources
-        holder.employeeId.text = item?.id.toString()
-        holder.firstname.text = item?.firstname
-        holder.lastname.text = item?.lastname
-        holder.gender.text = item?.gender
-        holder.dob.text = item?.dob
-        holder.address.text = item?.address
-        holder.country.text = item?.country
-        holder.designation.text = item?.designation
-        if(item?.id != null) {
-            val number = item.id
-            if(number % 2 == 0){
-                holder.itemView.setBackgroundColor(res.getColor(R.color.grey))
-            } else {
-                holder.itemView.setBackgroundColor(Color.WHITE)
-            }
-        }
+        holder.bind(item)
     }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater
-            .inflate(R.layout.list_item_employee, parent, false)
-
-        return ViewHolder(view)
+        return ViewHolder.from(parent)
     }
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    class ViewHolder private constructor(itemView: View): RecyclerView.ViewHolder(itemView){
         val employeeId: TextView = itemView.findViewById(R.id.employee_id)
         val firstname: TextView = itemView.findViewById(R.id.firstname)
         val lastname: TextView = itemView.findViewById(R.id.lastname)
@@ -55,5 +35,38 @@ class EmployeeListAdapter: RecyclerView.Adapter<EmployeeListAdapter.ViewHolder>(
         val address: TextView = itemView.findViewById(R.id.address)
         val country: TextView = itemView.findViewById(R.id.country)
         val designation: TextView = itemView.findViewById(R.id.designation)
+
+        fun bind(
+            item: EmployeeList.EmployeeResult?
+        ) {
+            // val res holds a reference to the resources for this view
+            val res = itemView.context.resources
+            employeeId.text = item?.id.toString()
+            firstname.text = item?.firstname
+            lastname.text = item?.lastname
+            gender.text = item?.gender
+            dob.text = item?.dob
+            address.text = item?.address
+            country.text = item?.country
+            designation.text = item?.designation
+            if (item?.id != null) {
+                val number = item.id
+                if (number % 2 == 0) {
+                    itemView.setBackgroundColor(res.getColor(R.color.grey))
+                } else {
+                    itemView.setBackgroundColor(Color.WHITE)
+                }
+            }
+        }
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater
+                    .inflate(R.layout.list_item_employee, parent, false)
+
+                return ViewHolder(view)
+            }
+        }
     }
+
 }
